fb709ed5eabbd2696c6b36e314f5521c416b8400
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2018-06-01 19:03:31 +0200

    implement test controller pull generator

--- RX_tests.py
+++ RX_tests.py
@@ -3,9 +3,20 @@ import redis
 import socket
 import errno
 import json
+import time
+import random
 
+# Configuration options
 MASTER_IP = '10.6.19.11'
+CONTROLLER_IP = '10.61.19.90'
+BROADCAST_DOMAIN = '<broadcast>'
 RECEIVE_PORT = 5000
+BROADCAST_PORT = 5566
+
+# Creating a socket to broadcast the controller changes
+broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+broadcast_socket.bind(('', 0))
+broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
 
 def _write_to_db(db, key, value):
@@ -17,8 +28,12 @@ def _read_from_db(db, key):
     db.get(key)
 
 
-def _pull_from_controller():
-    pass
+def _pull_from_channel(channel_nr):
+    while True:
+        # Simulating by now
+        yield 'channel' + str(channel_nr) + '=' + str(random.randint(0, 100))
+        # time.sleep(.001)
+
 
 
 def _write_to_controller(key, value):
@@ -26,7 +41,8 @@ def _write_to_controller(key, value):
 
 
 def _parse_key_value(s):
-    return json.loads(s)
+    return s.split('=')
+    # return json.loads(s)
 
 
 def _socket_generator(host, port, prefix_length=4):
@@ -41,7 +57,7 @@ def _socket_generator(host, port, prefix_length=4):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     except socket.error as e:
         if e.errno == errno.ECONNREFUSED:
-            # Handle the exception...
+            # TODO: handle the case where the master computer
             pass
         else:
             print('Failed to create socket. Error code: {}'.format(e.errno))
@@ -68,11 +84,14 @@ def _socket_generator(host, port, prefix_length=4):
         yield datagram
 
 
+def _broadcast_state(message, bc_socket):
+    bc_socket.sendto(message, (BROADCAST_DOMAIN, BROADCAST_PORT))
+
+
 def my_generator(n=10):
     # a generator that yields items instead of returning a list
     yield 'key=' + str(n)
     yield 'key=' + str(n+2)
-
 
 
 def main():
@@ -81,15 +100,19 @@ def main():
 
     # source = Observable.from_(['channel1:exp_time=0.123', 'channel2:ex_power=80.0']).publish()
     # source = Observable.from_(my_generator(10)).publish()
-    udp_source = Observable.from_(_socket_generator(MASTER_IP, RECEIVE_PORT)).publish()
+    # udp_source = Observable.from_(_socket_generator(MASTER_IP, RECEIVE_PORT)).publish()
+    controller_source = Observable.from_(_pull_from_channel(0)).publish()
 
+    # udp_mapped = udp_source.map(lambda s: _parse_key_value(s))
+    controller_mapped = controller_source.map(lambda s: _parse_key_value(s))
 
-    mapped = udp_source.map(lambda s: _parse_key_value(s))
+    # udp_mapped.subscribe(lambda x: _write_to_controller(x[0], x[1]))
+    # udp_mapped.subscribe(lambda x: _write_to_db('Redis', x[0], x[1]))
+    controller_mapped.subscribe(lambda x: _write_to_controller(x[0], x[1]))
+    controller_mapped.subscribe(lambda x: _write_to_db('Redis', x[0], x[1]))
 
-    mapped.subscribe(lambda x: _write_to_controller(x[0], x[1]))
-    mapped.subscribe(lambda x: _write_to_db('Redis', x[0], x[1]))
-
-    udp_source.connect()
+    # udp_source.connect()
+    controller_source.connect()
 
 
 if __name__=='__main__':
